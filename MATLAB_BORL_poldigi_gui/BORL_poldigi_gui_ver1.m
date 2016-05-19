0d6bb02aea45a4c41916b22c7789d8cbb4b19855
ReubenHill <reuben.hill@ucl.ac.uk>
2016-05-19 17:11:54 +0100

    Disallow user changing of atlas points for now

--- MATLAB_BORL_poldigi_gui/BORL_poldigi_gui_ver1.m
+++ MATLAB_BORL_poldigi_gui/BORL_poldigi_gui_ver1.m
@@ -602,29 +602,18 @@ data = get(handles.coords_table,'Data');
 % (doesn't if no selection performed before clicking or cell has been 
 % deselected)
 if(isfield(handles,'selectedRow'))
-    
     if(handles.selectedRow < 5)
-        % trying to insert into one of the landmark points
-        warningString = {'About to insert or delete a landmark point.'; ... 
-                    'Pressing OK will remove the ability to align atlas points...'}
-        button = questdlg(warningString,'Warning','OK','Cancel','default');
-        if(button == 'OK')
-            handles.CannotAlignAtlasPoints = true;
-        else
-            % user selected "Cancel"
-            set
-        end      
-    end
-    
-    % insert below selected row...
-    row = handles.selectedRow;
-    dataBelowSelectedRow = data(row+1:end,:);
-    % add new row by adding a single rowed cell array
-    data(row+1,:) = cell(1,size(data,2));
-    % add back the data that was saved before by concatenating below where
-    % the new row has been added.
-    data = [data(1:row+1,:) ; dataBelowSelectedRow];  
-    
+        errordlg('Cannot insert or delete Atlas Points','Error','modal');
+    else
+        % insert below selected row...
+        row = handles.selectedRow;
+        dataBelowSelectedRow = data(row+1:end,:);
+        % add new row by adding a single rowed cell array
+        data(row+1,:) = cell(1,size(data,2));
+        % add back the data that was saved before by concatenating below where
+        % the new row has been added.
+        data = [data(1:row+1,:) ; dataBelowSelectedRow];  
+    end    
 else
     % insert empty row at the end
     data{end+1,1} = [];
